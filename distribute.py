48ed5dabccea31891fbb92f460dd18d038651f1a
Elliott Dicus <ewdicus@gmail.com>
2013-11-12 17:38:08 -0800

    Simplified install.sh creation

--- distribute.py
+++ distribute.py
@@ -21,18 +21,21 @@ def getVersion(inputFilename):
         return version.strip()
 
 def createInstallScript(versionString):
-    with open("install_top.txt") as top, open("install_bottom.txt") as bottom, open("WireGardenPlugin/install.sh","w") as install:
-        install.write(top.read())
-        install.write('version="%s"\n' % versionString)
-        install.write(bottom.read())
+    with open("install.txt") as installTemplate, open("WireGardenPlugin/install.sh","w") as installScript:
+        for line in installTemplate:
+            if "# Replace line with version" in line:
+                print "ta-da"
+                installScript.write('version="%s"\n' % versionString)
+            else:
+                installScript.write(line)
 
 # if run as top-level script
 if __name__ == '__main__':
     # Copy WireGarden Source into source folder
-    copySource("../fbProjects/WireGardenPlugin","WireGardenPlugin/Source/WireGardenPlugin")
+    #copySource("../fbProjects/WireGardenPlugin","WireGardenPlugin/Source/WireGardenPlugin")
     # Version
     versionString = getVersion("version.txt")
     # Create install script with correct version
     createInstallScript(versionString)
     # Create Archive
-    createArchive("WireGardenPlugin", versionString)
+    #createArchive("WireGardenPlugin", versionString)
